39cce04d22c46d7ee37f1176429355e47797dd2a
tami1326 <111925383+tami1326@users.noreply.github.com>
2022-09-03 14:56:33 +0300

    Create search.js

---
+++ search.js
@@ -0,0 +1,9 @@
+function searchWord() {
+	var currentWord = document.getElementById('inputWord').value;
+	if (myList.includes(currentWord) == true) {
+		alert("We know this one already!");
+	} else {
+		alert("This is a new word!");
+	}
+	return false;
+}
